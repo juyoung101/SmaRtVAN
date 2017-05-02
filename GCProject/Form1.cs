86a4df54b135266fdcfe6bc06c9f583dd5c71788
kwuz <young.mikaila@gmail.com>
2017-05-01 20:48:29 -0600

    power display update functionality

--- GCProject/Form1.cs
+++ GCProject/Form1.cs
@@ -18,12 +18,27 @@ namespace GCProject
             InitializeComponent();
         }
 
-        private void Update_Remaining_Power(decimal power)
+        public void Update_Remaining_Power(decimal power)
         {
 
             Math.Round(power, 2);
             string p = power.ToString();
             CurrentStoredPower.Text = p + " MaH";
         }
+
+        public void Update_Power_Generation(decimal power)
+        {
+            Math.Round(power, 2);
+            string p = power.ToString();
+            CurrentPowerGeneration.Text = p + " MaH";
+        }
+
+        public void Update_Power_Expendeture(decimal power)
+        {
+            Math.Round(power, 2);
+            string p = power.ToString();
+            CurrentPowerExpendeture.Text = p + " MaH";
+        }
+
     }
 }
