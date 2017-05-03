b518cab9ba277ce9347770b2be4e5d1c3ef3d3d8
Justin Young <juyoung101@msn.com>
2017-05-03 12:16:28 -0600

    Restructured devices into DevicePackage. Solar and Wind now vary based on time of day, simulated for actual sensor values that determine solar and wind collection

---
+++ GCProject/DevicePackage/PowerInPackage/GeneratorCollector.cs
@@ -0,0 +1,24 @@
+﻿using GCProject.DevicePackage;
+using GCProject.SensorPackage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCProject.Collection
+{
+    public class GeneratorCollector : Device
+    {
+        public string energyProductionAmount { get; set; }
+
+        decimal read()
+        {
+            decimal energy = 0;
+            energy = Decimal.Parse(energyProductionAmount);
+            energy += (SensorHandler.getRandom() % 10) - 5;
+            makeHistory(energy);
+            return energy;
+        }
+    }
+}
---
+++ GCProject/DevicePackage/PowerInPackage/WindCollector.cs
@@ -0,0 +1,48 @@
+﻿using GCProject.DevicePackage;
+using GCProject.SensorPackage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCProject.Collection
+{
+    public class WindCollector : Device
+    {
+        public string energyProductionAmount { get; set; }
+        public iSensor time { get; set; }
+        public bool isTimeSet = false;
+
+        public void associateTimeSensor(iSensor a)
+        {
+            time = a;
+            isTimeSet = true;
+        }
+
+        decimal read()
+        {
+            decimal energy = 0;
+            energy = Decimal.Parse(energyProductionAmount);
+            if (isTimeSet)
+            {
+                var intTime = time.read().Split(':');
+                var hour = int.Parse(intTime[0]);
+                if (hour < 10 && hour > 4)
+                {
+                    energy += (SensorHandler.getRandom() % 10) - 1;
+                }
+                else
+                {
+                    energy -= (SensorHandler.getRandom() % 10) - 9;
+                }
+            }
+            else
+            {
+                energy -= (SensorHandler.getRandom() % 10) - 5;
+            }
+            makeHistory(energy);
+            return energy;
+        }
+    }
+}
